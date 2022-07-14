d1cacb04e37ecedefa0b51708970efa1e00227ed
Ramiro Martinez <ramiro.martinez@alu.bue.edu.ar>
2022-07-14 15:43:56 -0300

    Version con boton contador

---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+
+const ItemCount = ({ initial=1, stock=10, onAdd }) => {
+    const [count, setCount] = useState(1)
+    
+    const handleAdd = () => {
+    if (count < stock){ 
+    setCount(count + 1) 
+    }
+}
+
+    const handleRestar = () => {
+    if (count >1) {
+    setCount(count - 1) }
+    }
+
+
+const handleAddToCart = () => {
+    onAdd(count)    
+}    
+return (
+    <div className='card w-25'>
+        <label>{ count }</label>
+        <br />
+        <button className='btn btn-outLine-primary ' onClick={handleAdd }> + </button>
+        <button className='btn btn-outLine-primary 'onClick={handleRestar }> - </button>
+        <button className='btn btn-outLine-success' onClick={handleAddToCart}> Agregar Carrito</button>
+    </div>
+)
+}
+
+export default ItemCount
--- src/components/ItemListContainer/ItenListContainer.jsx
+++ src/components/ItemListContainer/ItenListContainer.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import ItemCount from '../ItemCount/ItemCount'
 
 const ItenListContainer = ({saludo}) => {
+const onAdd = (cant) => {
+    console.log(`La cantidad es: ${cant}`)
+}    
 return (
-    <div>{saludo}</div>
+    <div>{saludo}
+    <ItemCount initial={1} stock={10} onAdd={onAdd}  />
+    </div>
 )
 }
 
